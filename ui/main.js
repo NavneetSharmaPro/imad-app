318fe8aeaf37198e28c890e734e3a3a1acbd154a
Navneet <NavneetSharmaPro@users.noreply.github.com>
2018-03-12 18:02:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,3 +5,10 @@ console.log('Loaded!');
 var element = document.getElementById('main-text');
 
 element.innerHTML = 'New Value';
+
+//making madi move
+
+var img = document.getElementById('madi');
+img.onclick = function () {
+    img.style.marginLeft='100px';
+}
