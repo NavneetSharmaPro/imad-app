d4dcdcfbc9dd9f2ef65c28deffe31a9ef16197e1
Navneet <NavneetSharmaPro@users.noreply.github.com>
2018-03-12 18:32:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -98,7 +98,11 @@ app.get('/:articleName',function(req,res){
     //This t in createTemplate fucked me over. -_-
 });
 
-
+var counter = 0;
+app.get('/counter', function(req, res) {
+   counter = counter+1;
+   res.send(counter.toString());
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
