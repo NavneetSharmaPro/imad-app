5eddd3e6dbd4f626dd038efe8e925e549b3d4a77
Navneet <NavneetSharmaPro@users.noreply.github.com>
2018-03-13 01:42:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,5 @@
+/* Old but loved these codes
+
 console.log('Loaded!');
 
 //change the text of the main-text div
@@ -18,3 +20,20 @@ img.onclick = function () {
     var interval = setInterval(moveRight, 50);
     
 }
+
+*/
+
+//Counter Code
+var button = document.getElementById('counter');
+var counter = 0;
+
+button.onclick = function() {
+     //make a req to counter endpoint
+     
+     //capture the response and store it in a variable
+     
+     //render the variable in the correct span
+     counter = counter + 1;
+     var span = document.getElementById('count');
+     span.innerHTML = counter.toString();
+};
