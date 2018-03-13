04f8185282b081b0c1fef64bedca01c7d7c57e7c
Navneet <NavneetSharmaPro@users.noreply.github.com>
2018-03-14 01:03:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -90,19 +90,19 @@ function createTemplate(data){
 }
 //Added new app.get() * 3
 
+var counter = 0;
+app.get('/counter', function(req, res) {
+   counter = counter + 1;
+   res.send(counter.toString());
+});
+
+
 app.get('/:articleName',function(req,res){
     //articleName==article-one
     //articles[articleName]=={} content onject for article
     var articleName=req.params.articleName;
     res.send(createTemplate(articles[articleName]));
     //This t in createTemplate fucked me over. -_-
-});
-
-
-app.get('/counter', function(req, res) {
-    var counter = 0;
-   counter = counter + 1;
-   res.send(counter.toString());
 });
 
 app.get('/ui/style.css', function (req, res) {
